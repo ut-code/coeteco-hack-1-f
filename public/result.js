6ab845da84bb4ef574ec4566ba915b56743845eb
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2023-12-10 18:13:51 +0900

    result

--- public/result.js
+++ public/result.js
@@ -1,30 +1,69 @@
-const storedValue = localStorage.getItem('storedValue');
-const answer = "option1";
-const description = "解説";
-const radioButtons = document.getElementsByName('answer');
-const descriptionElement = document.getElementById('description');
+const questionJson = JSON.parse(localStorage.getItem("question"));
+const numberOfQuestions = questionJson.length;
+//const answer = "option1";
+//const description = "解説";
+const questionsContainer = document.getElementById('questions-container');
+questionsContainer.innerHTML = '';
 
-if (storedValue) {
-  for (let i = 0; i < radioButtons.length; i++) {
-    if (radioButtons[i].value === storedValue) {
-      radioButtons[i].checked = true;
-      
-      for (let j = 0; j < radioButtons.length; j++) {
-        radioButtons[j].disabled = true;
-      }
+function setupQuestions() {
+    for (let i = 1; i <= numberOfQuestions; i++) {
+        const storedValue = localStorage.getItem('storedValue-' + i);
+        const questionDiv = document.createElement('div');
+        questionDiv.classList.add('question');
 
-      radioButtons[i].closest('label').style.backgroundColor = (storedValue === answer) ? "rgba(0, 255, 0, 0.8)" : "rgba(255, 0, 0, 0.8)";
-      radioButtons[i].closest('label').style.borderRadius = '10px';      
+        const questionText = document.createElement('div');
+        questionText.id = 'question-' + i;
+        questionText.textContent = questionJson[i].q;
 
-      // id="description"のtextContentを設定
-      descriptionElement.style.fontSize = '30px';
-      descriptionElement.innerHTML = (storedValue === answer.toString())
-      ? "正解！<br>" + description
-      : "正解は " + answer + "<br>" + description;
+        const answersContainer = document.createElement('div');
+        answersContainer.id = 'answers-' + i;
 
-      break;
+        // ここでdescriptionElementを作成
+        const descriptionElement = document.createElement('div');
+        descriptionElement.id = 'description-' + i;
+
+        for (let j = 1; j <= 4; j++) {
+            const label = document.createElement('label');
+            label.classList.add('radio');
+
+            const input = document.createElement('input');
+            input.type = 'radio';
+            input.name = 'answer-' + i;
+            input.value = 'option' + j;
+
+            const text = document.createTextNode('option ' + j);
+
+            label.appendChild(input);
+            label.appendChild(text);
+            answersContainer.appendChild(label);
+
+            // ラジオボタンの選択状態を設定
+            if (input.value === storedValue) {
+                input.checked = true;
+            }
+
+            // 選択されたラジオボタンに対する処理
+            if (storedValue && input.checked) {
+                input.disabled = true;
+                label.style.backgroundColor = (storedValue === answer) ? "rgba(0, 255, 0, 0.8)" : "rgba(255, 0, 0, 0.8)";
+                label.style.borderRadius = '10px';
+
+                // id="description"のtextContentを設定
+                descriptionElement.style.fontSize = '20px';
+                descriptionElement.innerHTML = (storedValue === answer.toString())
+                    ? "正解！<br>" + description
+                    : "正解は " + answer + "<br>" + description;
+            }
+        }
+
+        questionDiv.appendChild(questionText);
+        questionDiv.appendChild(answersContainer);
+        
+        // descriptionElementを追加
+        questionDiv.appendChild(descriptionElement);
+
+        questionsContainer.appendChild(questionDiv);
     }
-  }
 }
 
 const redirectToHomeButton = document.getElementById('redirectToHome');
@@ -34,9 +73,11 @@ const regenerateQuestionButton = document.getElementById('regenerateQuestion');
 regenerateQuestionButton.addEventListener('click', regenerateQuestion);
 
 function redirectToHome() {
-  window.location.href = 'index.html';
+    window.location.href = 'index.html';
 }
 
 function regenerateQuestion() {
-  window.location.href = 'form.html';
+    window.location.href = 'form.html';
 }
+
+setupQuestions();
