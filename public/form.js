387864928c0fb01632bd21d254cf10ab434a9da2
entitledes <137141052+entitledes@users.noreply.github.com>
2023-12-10 14:49:42 +0900

    問題数と難易度を渡せるようにした

---
+++ public/form.js
@@ -0,0 +1,4 @@
+const storedNumberOfQuestion = localStorage.getItem('numberOfQuestion');
+const storedDifficultyOfQuestion = localStorage.getItem('difficultyOfQuestion');
+
+console.log(storedNumberOfQuestion);
